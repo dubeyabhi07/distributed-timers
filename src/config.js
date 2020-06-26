28690fb6e5de8f4ffc10a589d4ed7526656e8949
Abhishek Dubey <dubey07.abhishek@gmail.com>
2020-06-26 18:39:12 +0530

    added configuration and table schema

--- src/config.js
+++ src/config.js
@@ -1,1 +1,29 @@
-// TODO : set config as per user input
+
+var params = {
+};
+
+exports.setConfig = function (_params) {
+  return new Promise(function (resolve, reject) {
+    if (_params.database === 'dynamoDB') {
+      params.isLocal = _params.isLocal;
+      if (!params.isLocal) {
+        params.accessKeyId = _params.accessKeyId;
+        params.secretAccessKey = _params.secretAccessKey;
+      }
+      params.region = _params.region;
+      params.endpoint = _params.endpoint;
+      params.tableName = _params.tableName;
+      params.readCapacityUnitsForGSI = _params.readCapacityUnitsForGSI;
+      params.writeCapacityUnitsForGSI = _params.writeCapacityUnitsForGSI;
+      params.readCapacityUnits = _params.readCapacityUnits;
+      params.writeCapacityUnits = _params.writeCapacityUnits;
+      params.scanPeriod = _params.scanPeriod;
+      params.verboseLogging = _params.verboseLogging;
+    }
+    resolve();
+  });
+};
+
+exports.getConfig = function () {
+  return params;
+};
--- src/database/dynamoDB/dbconfig.js
+++ src/database/dynamoDB/dbconfig.js
@@ -1,1 +1,96 @@
-//TODO : write database configurations
+'use strict';
+
+const logger = require('../../logger');
+const log = logger.logger;
+const configParams = require('../../config.js');
+const AWS = require('aws-sdk');
+
+var awsConfig = {
+  accessKeyId: configParams.getConfig().accessKeyId,
+  secretAccessKey: configParams.getConfig().secretAccessKey,
+  region: configParams.getConfig().region
+};
+awsConfig.endpoint = configParams.getConfig().endpoint;
+AWS.config.update(awsConfig);
+
+var client = new AWS.DynamoDB();
+
+var documentClient = new AWS.DynamoDB.DocumentClient();
+
+exports.getDynamoDbClient = function () {
+  return documentClient;
+};
+
+exports.createTable = function () {
+  return new Promise(function (resolve, reject) {
+    var tableName = configParams.getConfig().tableName;
+
+    var tableNameParams = {
+      TableName: tableName,
+      KeySchema: [{
+        AttributeName: 'id',
+        KeyType: 'HASH'
+      }
+      ],
+
+      AttributeDefinitions: [{
+        AttributeName: 'id',
+        AttributeType: 'S'
+      }, {
+        AttributeName: 'expireTime',
+        AttributeType: 'N'
+      }, {
+        AttributeName: 'GSI',
+        AttributeType: 'S'
+      }],
+      GlobalSecondaryIndexes: [
+        {
+          IndexName: '_expire',
+          KeySchema: [
+            {
+              AttributeName: 'GSI',
+              KeyType: 'HASH'
+            },
+            {
+              AttributeName: 'expireTime',
+              KeyType: 'RANGE'
+            }
+          ],
+          ProvisionedThroughput: {
+            ReadCapacityUnits: configParams.getConfig().readCapacityUnitsForGSI,
+            WriteCapacityUnits: configParams.getConfig().writeCapacityUnitsForGSI
+          },
+          Projection: {
+            ProjectionType: 'ALL'
+          }
+        }
+
+      ],
+      ProvisionedThroughput: {
+        ReadCapacityUnits: configParams.getConfig().readCapacityUnits,
+        WriteCapacityUnits: configParams.getConfig().writeCapacityUnits
+      }
+    };
+
+    /**
+         * Creates table, if it is not created already.
+         * @param tableErr
+         * @param tableData
+         * @returns
+         */
+    client.createTable(tableNameParams, function (tableErr, tableData) {
+      if (tableErr) {
+        if (tableErr.code === 'ResourceInUseException') {
+          log.info(logger.printLog(`${tableName} table exists already !`));
+        } else {
+          log.info(logger.printLog(`table creation Error ${JSON.stringify(tableErr, null, 2)}`));
+          reject(tableErr);
+        }
+        resolve();
+      } else {
+        log.info(logger.printLog(`${tableName} Created table successfully!`));
+        resolve();
+      }
+    });
+  });
+};
